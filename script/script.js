e5222b8f5ca20f913a004b8bcb403844f62e8b90
Soya Beans <5205954@student.gloscol.ac.uk>
2020-01-17 09:55:40 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,1 @@
-function throwdice() {
-  var randomdice = Math.floor(Math.random() * 6 + 1);
-  document
-    .getElementById("imageLocation")
-    .setAttribute("src", "../images/d" + randomdice + ".png");
-}
+
