4779f65546d1356434e1c43c22c4e85a7da310b8
Soya Beans <5205954@student.gloscol.ac.uk>
2020-01-17 09:47:20 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,6 +4,6 @@ function throwdice() {
     .getElementById("imageLocation")
     .setAttribute(
       "src",
-      "https://cdn.glitch.com/63106535-e105-42c9-8ec5-e00e3d6ac851%2Fd1.png?1544998233632"
+      "../images/d" + randomdice + ".png"
     );
 }
